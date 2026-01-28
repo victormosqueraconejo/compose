f653293fec23421ae2a542846d883b52c5c0421f
victormosqueraconejo <victormanuelmosqueraconejo@gmail.com>
2026-01-28 09:29:01 -0500

    Circular Progress Indicator

--- app/src/main/java/com/victor/compose/MainActivity.kt
+++ app/src/main/java/com/victor/compose/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.victor.compose.ui.theme.ComposeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LinearDeterminateIndicator()
                 }
             }
         }
@@ -31,17 +44,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LinearDeterminateIndicator() {
+    var progresoActual by remember { mutableFloatStateOf(0f) }
+    var cargando by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope ()
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            cargando = true
+            scope.launch {
+                cargarProgreso { progreso ->
+                    progresoActual = progreso
+                }
+                cargando = false
+            }
+        }, enabled = !cargando) {
+            Text("Start Loading")
+        }
+
+        if (cargando ) {
+            CircularProgressIndicator(
+                progress = {progresoActual},
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+
+    
+}
+
+suspend fun cargarProgreso(updateProgress: (Float) -> Unit) {
+    for (i in 1..100) {
+        updateProgress(i.toFloat() / 100)
+        delay(100)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeTheme {
-        Greeting("Android")
+        LinearDeterminateIndicator()
     }
 }
